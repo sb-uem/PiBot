1daaea1a0bfe7556a20c3e6021833380f5d15df5
daviagonilha <davi.agonilha22@gmail.com>
2017-07-11 19:08:50 -0300

    Update encoder.py

--- encoder.py
+++ encoder.py
@@ -10,3 +10,25 @@ def encoder_confere(estadoatual,i,alfa):
 def encoder_zera(i,alfa):  
   i = 0
   alfa = 0
+  
+def encoder_conferedir(estadoatual,i,alfa):
+  if ((estadoatual) != (estadoanterior)):
+    i =i+1
+    alfa = i*18
+    estadoanterior = estadoatual
+    estadoatual = leitura
+
+def encoder_zeradir(i,alfa):  
+  i = 0
+  alfa = 0    
+    
+def encoder_confereesq(estadoatual,i,alfa):
+  if ((estadoatual) != (estadoanterior)):
+    i =i+1
+    alfa = i*18
+    estadoanterior = estadoatual
+    estadoatual = leitura
+    
+def encoder_zeraesq(i,alfa):  
+  i = 0
+  alfa = 0
