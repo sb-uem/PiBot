81b7fd3e11f8f5bf6959e8ad7d1f407430478743
jair <jgcertorio@uol.com.br>
2017-07-11 19:04:19 -0300

    Update encoder.py

--- encoder.py
+++ encoder.py
@@ -1,12 +1,12 @@
 estadoanterior = LOW
-estadoatual = leitura
+
 def encoder_confere(estadoatual,i,alfa):
   if ((estadoatual) != (estadoanterior)):
-    i =i+1;
-    alfa = i*18;
-    estadoanterior = estadoatual;
-    estadoatual = leitura;
+    i =i+1
+    alfa = i*18
+    estadoanterior = estadoatual
+    estadoatual = leitura
  
 def encoder_zera(i,alfa):  
-  i = 0;
-  alfa = 0;
+  i = 0
+  alfa = 0
