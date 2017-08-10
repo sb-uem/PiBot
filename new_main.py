22661b5f1321ee0b08b168f7d96a4ed39dc740d8
jair <jgcertorio@uol.com.br>
2017-08-10 15:25:17 -0300

    Create new_main.py
    o futuro main, usando os módulos que criamos com o tempo

---
+++ new_main.py
@@ -0,0 +1,19 @@
+import encoder
+import Motor
+import servoDMA
+
+
+
+#inicializa
+
+#cria os dois encoders
+
+#cria os dois motores
+
+#cria o servo da câmera
+
+#começa a Curses e começa o loop
+
+
+
+
