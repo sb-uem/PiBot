4e35834d57c59e10b212fd753609da3dcb2b69af
luccacondratiuk <luccacondratiuk@gmail.com>
2017-08-10 15:23:47 -0300

    Adicionado PWM via DMA para controle do Servo da Câmera

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import RPi.GPIO as GPIO
 import curses
 import thread
 import time
+from RPIO import PWM
 
 Enable1=22 #Enable Motor 1
 Enable2=25 #Enable Motor 2
+ServoCam=13 #Controle Servo da Câmera
 
 MD1=27  #MotorDireitoEntrada1
 MD2=17  #MotorDireitoEntrada2
@@ -22,7 +24,18 @@ GPIO.setup(ME2 ,GPIO.OUT) # IN4
 #Set Enable High
 Enable1=1
 Enable2=1
+#//////////////CONTROLE DIREÇÃO CÂMERA\\\\\\\\\\\\\
+def camdireita():
+	PWM.add_channel_pulse(0, ServoCam, 100, 220 )
 
+def camesquerda():
+	PWM.add_channel_pulse(0, ServoCam, 100, 5 )
+
+def camcentro():
+	PWM.add_channel_pulse(0, ServoCam, 100, 120 )
+
+
+#/////////////CONTROLE MOVIMENTAÇÃO\\\\\\\\\\\\\\\
 def acelerar(tempo,d):
    t=float(tempo)
    if d==1:
@@ -73,13 +86,19 @@ def PARAR():
 	# MOTOR 2
 	GPIO.output(ME1 ,1) # IN3
 	GPIO.output(ME2 ,1) # IN4
+	
 
-stdscr = curses.initscr()
-curses.noecho()
-curses.cbreak()
-stdscr.keypad(1)
-stdscr.refresh()
+#////////////MAIN\\\\\\\\\
+#---INICIALIZAÇÃO
+stdscr = curses.initscr() #Inicia Curses
+curses.noecho()  #Desabilita Verbose Terminal - Curses
+curses.cbreak()  #Habilita terminal para apenas um caractere - Curses
+stdscr.keypad(1) #Habilita Uso do Teclado Numérico - Curses
+stdscr.refresh() #Atualiza Tela - Curses
+PWM.setup() #Inicia PWM - RPIO/PWM
+PWM.init_channel(0) #Inicia Canal PWM para Servo Câmera - RPIO/PWM
 
+#---LOOP PRINCIPAL
 tecla = ''
 while tecla != ord('q'):
    tecla = stdscr.getch()
@@ -95,10 +114,19 @@ while tecla != ord('q'):
    elif tecla == curses.KEY_RIGHT:
       dire=0
       rota(8,dire)
-      
-stdscr.keypad(0)
-curses.nocbreak()
-curses.echo()
-curses.endwin()
-GPIO.cleanup()
+   elif tecla == ord('a'):
+	camesquerda()
+   elif tecla == ord('d'):
+	camdireita()
+   elif tecla == ord('s):
+	camcentro()
+	
+#---FINALIZAÇÃO
+PWM.clear_channel_gpio(0, ServoCam)  #Limpa Canal 0 do Servo da Câmera
+PWM.cleanup() # Limpa Pinos de PWM
+stdscr.keypad(0) #Desabilita Teclado Numérico
+curses.nocbreak() #Desabilita Curses
+curses.echo()     #Habilita Verbose Terminal
+curses.endwin()   #Desliga Biblioteca Curses
+GPIO.cleanup()    #Limpa Pinos GPIO
 
