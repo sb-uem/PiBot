9b6752c8e59539fc8eb5b04215f30b53af0e3a44
tsshiroshima <thiago.shiroshima@gmail.com>
2017-08-11 12:57:42 -0300

    Update Motor.py
    Modificado para declarar os motores individualmente

--- Motor.py
+++ Motor.py
@@ -3,48 +3,21 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 
-Enable1=22 #Enable Motor 1
-Enable2=25 #Enable Motor 2
-
-#Set Enable High
-GPIO.output(Enable1,1)
-GPIO.output(Enable2,1)
-
 class Motor(object):
 	
-	def __init__(self,md1,md2,me1,me2):
-		self.md1=md1 #MotorDireitoEntrada1
-		self.md2=md1 #MotorDireitoEntrada2
-		self.me1=me1 #MotorEsquerdoEntrada1
-		self.me2=me2 #MotorEsquerdoEntrada2
+	def __init__(self,pino1,pino2):
+		self.pino1=pino1 #Pino 1 do motor
+		self.pino2=pino2 #Pino 2 do motor
 		self.parar()
 
 	def frente(self):
-		GPIO.output(self.md1,1)
-		GPIO.output(self.md2,0)
-		GPIO.output(self.me1,1)	
-		GPIO.output(self.me2,0)
+		GPIO.output(self.pino1,1)
+		GPIO.output(self.pino2,0)
 
 	def re(self):
-		GPIO.output(self.md1,0)
-		GPIO.output(self.md2,1)
-		GPIO.output(self.me1,0)
-		GPIO.output(self.me2,1)
-
-	def direita(self):
-		GPIO.output(self.md1,0)
-		GPIO.output(self.md2,1)
-		GPIO.output(self.me1,1)
-		GPIO.output(self.me2,0)
-
-	def esquerda(self):
-		GPIO.output(self.md1,1)
-		GPIO.output(self.md2,0)
-		GPIO.output(self.me1,0)
-		GPIO.output(self.me2,1)		
-
+		GPIO.output(self.pino1,0)
+		GPIO.output(self.pino2,1)
+		
 	def parar(self):
-		GPIO.output(self.md1,1)
-		GPIO.output(self.md2,1)
-		GPIO.output(self.me1,1)
-		GPIO.output(self.me2,1)
+		GPIO.output(self.pino1,1)
+		GPIO.output(self.pino2,1)
