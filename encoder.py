18efc3eb0ebcbbb502c2bfbe063f8a45bdff059e
jcert <jgcertorio@uol.com.br>
2017-09-21 18:46:12 -0300

    encoder class fix

--- encoder.py
+++ encoder.py
@@ -6,8 +6,6 @@ class Encoder(object):
         self.estadoanterior = False
         self.pino = pino
         self.encoder_zera()
-        
-        
       
   def encoder_confere(self):
     self.estadoatual = GPIO.input(self.pino);  
@@ -15,43 +13,9 @@ class Encoder(object):
       self.i = self.i+1
       self.alfa = self.i*18
       self.estadoanterior = self.estadoatual
-      
    
   def encoder_zera(self):  
    self.i = 0
    self.alfa = 0
   
  
-
-def encoder_confere(estadoatual,i,alfa):
-  if ((estadoatual) != (estadoanterior)):
-    i =i+1
-    alfa = i*18
-    estadoanterior = estadoatual
-    estadoatual = leitura
- 
-def encoder_zera(i,alfa):  
-  i = 0
-  alfa = 0
-  
-def encoder_conferedir(estadoatual,i,alfa):
-  if ((estadoatual) != (estadoanterior)):
-    i =i+1
-    alfa = i*18
-    estadoanterior = estadoatual
-    estadoatual = leitura
-
-def encoder_zeradir(i,alfa):  
-  i = 0
-  alfa = 0    
-    
-def encoder_confereesq(estadoatual,i,alfa):
-  if ((estadoatual) != (estadoanterior)):
-    i =i+1
-    alfa = i*18
-    estadoanterior = estadoatual
-    estadoatual = leitura
-    
-def encoder_zeraesq(i,alfa):  
-  i = 0
-  alfa = 0
